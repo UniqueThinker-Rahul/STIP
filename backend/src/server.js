373b3f56dc5b239f71f55dff4f63d38567b84bad
Rahul Kumawat <rl988792@gmail.com>
2026-05-26 21:19:54 +0530

    final changes before rebase

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://your-frontend.vercel.app' 
+    'https://stipdash.vercel.app/' 
   ],
   credentials: true
 }));
